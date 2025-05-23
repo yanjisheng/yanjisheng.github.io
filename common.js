86ab65f6ff9dc2d18392e46cecf09d45eaf9667a
颜姬 <36791327+yanjisheng@users.noreply.github.com>
2025-05-23 16:51:09 +0800

    Add files via upload

---
+++ common.js
@@ -0,0 +1,39 @@
+function f(){
+    let width = document.body.clientWidth;
+    let products = document.getElementsByClassName("product");
+    if(products.length <= 0){
+        return -1;
+    }
+    if(width < 660){
+        for(let i=0; i<products.length; i++){
+            products[i].style.display="block";
+            products[i].style.width=width-40+"px";
+            products[i].style.margin="10px auto";
+            products[i].lastChild.lastChild.style.width=width-160+"px";
+        }
+    }else{
+        for(let i=0; i<products.length; i++){
+            products[i].style.display="inline-block";
+            products[i].style.width="293px";
+            products[i].style.margin="10px 10px";
+            products[i].lastChild.lastChild.style.width="178px";
+        }
+    }
+}
+var r = f();
+if(r !== -1){
+    window.onresize = f;
+}
+function language(){
+    alert("English version currently not available.");
+}
+function search(){
+    keyword = document.getElementById("keyword").value;
+    window.open("https://cn.bing.com/search?q="+keyword+"+site%3amthorizon.com");
+}
+document.getElementById("search").onclick = search;
+document.getElementById("keyword").onkeydown = function(e){
+    if(e.key === "Enter"){
+        search();
+    }
+}
